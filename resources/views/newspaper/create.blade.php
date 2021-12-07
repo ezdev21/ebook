2bb3b4ba4cfce34b2622b0ed645b8d2afcbebdb8
gech11 <getachewfikadu77@gmail.com>
2021-12-08 00:10:37 +0300

    megazine and newspaper views included

---
+++ resources/views/newspaper/create.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+@section('content')
+ <div>
+  <form action="" enctype="multipart/form-data">
+  <input type="text" placeholder="title" class="">
+  <input type="file">
+  <input type="file">
+  <input type="submit">
+  </form>
+ </div>   
+@endsection
