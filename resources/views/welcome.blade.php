65f716065c67d5cd7382c36dbf0c81f814ee1d6b
gech11 <getachewfikadu77@gmail.com>
2021-12-12 23:59:41 +0300

    genres included

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,22 @@
  <div class="bg-liberary bg-fixed w-full h-full bg-cover">
   <div class="absolute top-1/3 right-1/4 p-5 bg-red-800 opacity-75 w-1/3 rounded-3xl">
     <search-component/>
-  </div>   
+  </div> 
+  <div>
+    @foreach ($categories as $category)
+     <div>
+     <h1>{{$category->name}}</h1>
+     <div>
+     @foreach ($category->books as $book)
+      <div>
+       <img src="/storage/books/{{$book->photo}}" class="w-48">
+       <p>{{$book->title}}</p>
+       <p>{{$book->author->name}}</p>  
+      </div>         
+     @endforeach  
+     </div>  
+     </div>   
+    @endforeach
+  </div>  
  </div>   
 @endsection
