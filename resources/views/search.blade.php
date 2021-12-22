7a42197a6c976e3a22c81b4d56a67e26e48c7d04
gech11 <getachewfikadu77@gmail.com>
2021-12-23 00:17:32 +0300

    search layout changed

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.app')
 @section('content')
-  <div class="flex flex-initial">
-    <div>
-     <h1>Books</h1>
+  <div class="flex flex-initial lg:w-11/12 mx-auto my-5">
+    <div class="w-1/3">
+     <h1 class="text-xl font-medium mb-2">Books</h1>
      @forelse ($books as $book)
      <div>
       <img src="/storage/books/{{$book->photo}}" class="w-48">
@@ -13,8 +13,8 @@
       <p class="text-2xl text-red-500">sorry! no book found</p>   
      @endforelse
     </div>
-    <div>
-     <h1>Newspapers</h1>
+    <div class="w-1/3">
+     <h1 class="text-xl font-medium mb-2">Newspapers</h1>
      @forelse ($newspapers as $newspaper)
     <div>
      <img src="/storage/newspapers/{{$newspaper->photo}}" class="w-48">
@@ -25,8 +25,8 @@
     <p class="text-2xl text-red-500">sorry! no newspaper found</p>   
    @endforelse
     </div>
-    <div>
-      <h1>Megazines</h1>
+    <div class="w-1/3">
+      <h1 class="text-xl font-medium mb-2">Megazines</h1>
       @forelse ($megazines as $megazine)
     <div>
      <img src="/storage/megazines/{{$megazine->photo}}" class="w-48">
