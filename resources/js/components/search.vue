5094534d87f9e7829dc06eb5bcf3496ffeb9b6a5
gech11 <getachewfikadu77@gmail.com>
2021-12-23 11:09:24 +0300

    liberary service feature added

--- resources/js/components/search.vue
+++ resources/js/components/search.vue
@@ -3,7 +3,7 @@
    <h1 class="text-2xl text-center mb-2">Search</h1>
    <form @submit.prevent="search" method="POST" class="flex items-center">
      <p class="mx-2 text-2xl">Search The</p>
-     <select v-model="searchType" class="text-gray-700 p-2 text-xl border-r-2 border-first">
+     <select v-model="searchType" class="text-gray-700 p-2.5 text-xl border-r-2 border-first">
        <option value="catalog" selected>Catalog</option>
        <option value="book">Books</option>
        <option value="megazine">Megazines</option>
@@ -11,7 +11,7 @@
        <option value="event">Events</option>
      </select>
      <p class="mx-2 text-2xl">By</p>
-     <select v-model="searchBy" class="text-gray-700 p-2 text-xl border-r-2 border-first">
+     <select v-model="searchBy" class="text-gray-700 p-2.5 text-xl border-r-2 border-first">
        <option value="keyword" selected>keyword</option>
        <option value="author">Author</option>
        <option value="title">Title</option>
@@ -21,7 +21,7 @@
        <option value="user">User</option>
      </select>
      <input type="text" v-model="query" required class="text-second w-96 py-2.5 px-3 text-lg lg:text-xl outline-none focus:border-b-2 border-second" placeholder="search...">
-     <button type="submit" class="bg-second capitalize rounded-r-3xl lg:rounded-r-3xl xl:rounded-r-3xl 2xl:rounded-r-3xl text-xl lg:text-3xl xl:text-2xl 2xl:text-2xl py-2.5 px-5 my-auto">
+     <button type="submit" class="bg-second capitalize rounded-r-3xl lg:rounded-r-3xl text-xl lg:text-3xl py-2.5 px-5 my-auto">
       <svg xmlns="http://www.w3.org/2000/svg" class="text-white h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
       </svg>
